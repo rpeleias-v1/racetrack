e22490bfd3c58e80234ac1509848b28510aa2d9d
Rodrigo Peleias <Rodrigo Peleias>
2013-02-02 15:59:42 -0200

    New files

--- grails-app/domain/racetrack/User.groovy
+++ grails-app/domain/racetrack/User.groovy
@@ -2,6 +2,17 @@ package racetrack
 
 class User {
 
+	String name
+	String password
+	String role = "user"
+
     static constraints = {
+    	name(blank:false, nullable:false, unique:true)
+    	password(password:true)
+    	role(inList:["user", "admin"])
+    }
+
+    String toString() {
+    	login
     }
 }
