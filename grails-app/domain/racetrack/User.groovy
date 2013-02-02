3306eca066ff9aba38f525efc921718c769c000e
Rodrigo Peleias <Rodrigo Peleias>
2013-02-02 15:35:12 -0200

    Add generated User scaffold files

--- grails-app/domain/racetrack/User.groovy
+++ grails-app/domain/racetrack/User.groovy
@@ -2,17 +2,6 @@ package racetrack
 
 class User {
 
-	Stirng login
-	Stirng password
-	Stirng role = "user"
-
     static constraints = {
-    	login(blank:false, nullable:false, unique:true)
-    	password(blank:false, password:true)
-    	role(inList:["admin", "user"])
-    }
-
-    String toString() {
-    	login
     }
 }
